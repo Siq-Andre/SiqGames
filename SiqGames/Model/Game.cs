097d7dc6876dcdaaff623480a7a19189a4415694
André Siqueira <andre.siqueira@itlab.com.br>
2024-10-30 13:14:14 -0300

    Added model for game and game price

---
+++ SiqGames/Model/Game.cs
@@ -0,0 +1,10 @@
+﻿namespace SiqGames.Model
+{
+    public class Game
+    {
+        public int GameId { get; set; }
+        public string Name { get; set; }
+        public Studio Studio { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ SiqGames/Model/GamePrice.cs
@@ -0,0 +1,10 @@
+﻿namespace SiqGames.Model
+{
+    public class GamePrice
+    {
+        public int GamePriceID { get; set; }
+        public double Price { get; set; }
+        public Game Game { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
