daafa818d690896ae2492e352c61b6e6dd20dc60
André Siqueira <andre.siqueira@itlab.com.br>
2024-10-30 11:56:05 -0300

    Created Context

---
+++ SiqGames/Database/SiqGamesContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SiqGames.Model;
+
+namespace SiqGames.Database
+{
+    public class SiqGamesContext: DbContext
+    {
+        public SiqGamesContext(): base() { }
+
+        public DbSet<Player> Player { get; set; }
+        public DbSet<Studio> Studio { get; set; }
+        public DbSet<Genre> Genre { get; set; }
+    }
+}
