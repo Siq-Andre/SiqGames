7b3a4a0298cd9f7407ad7f1bcdf838a182ae2ff3
André Siqueira <andre.siqueira@itlab.com.br>
2024-10-30 11:43:23 -0300

    Created player model

---
+++ SiqGames/Model/Player.cs
@@ -0,0 +1,11 @@
+﻿namespace SiqGames.Model
+{
+    public class Player
+    {
+        public int PlayerId { get; set; }
+        public string Nickname { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public DateOnly BirthDate { get; set; }
+    }
+}
