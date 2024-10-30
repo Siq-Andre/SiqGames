11dbeb7cc9ddc45e7616fb2e7b8ba666e011167b
André Siqueira <andre.siqueira@itlab.com.br>
2024-10-30 13:16:19 -0300

    Added model for Sale

---
+++ SiqGames/Model/Sale.cs
@@ -0,0 +1,10 @@
+﻿namespace SiqGames.Model
+{
+    public class Sale
+    {
+        public int SaleId { get; set; }
+        public Player Player { get; set; }
+        public GamePrice GamePrice { get; set; }
+        public double FinalPrice { get; set; }
+    }
+}
