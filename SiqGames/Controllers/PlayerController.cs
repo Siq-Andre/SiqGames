fe850658414913f1a89371fad9ff90bb6032b11f
André Siqueira <andre.siqueira@itlab.com.br>
2024-11-06 15:06:34 -0300

    created player controller

---
+++ SiqGames/Controllers/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SiqGames.Database;
+using SiqGames.Model;
+
+namespace SiqGames.Controllers
+{
+    [ApiController]
+    [Route("[Controller]")]
+    public class PlayerController : Controller
+    {
+
+        private SiqGamesContext _context;
+
+        public PlayerController(SiqGamesContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public IActionResult AddPlayer([FromBody] Player player)
+        {
+            if (player == null)
+            {
+                return BadRequest("Player data is required.");
+            }
+
+            try
+            {
+                _context.Players.Add(player);
+                _context.SaveChanges();
+                return CreatedAtAction(nameof(AddPlayer), new { id = player.PlayerId }, player);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Internal server error while adding player.");
+            }
+        }
+
+        [HttpGet("select")]
+        public ActionResult<IEnumerable<Player>> SelectPlayers()
+        {
+            try
+            {
+                var players = _context.Players.ToList();
+                return Ok(players);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Internal server error while retrieving players.");
+            }
+        }
+
+    }
+}
