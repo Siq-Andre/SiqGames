305244d9e5e76acbe2ee8c1d76826eec2cb331d1
André Siqueira <andre.siqueira@itlab.com.br>
2024-11-07 15:59:30 -0300

    Added DAL for basci CRUD

--- SiqGames/Program.cs
+++ SiqGames/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using SiqGames.Database;
+using SiqGames.Model;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,6 +16,8 @@ builder.Services.AddDbContext<SiqGamesContext>(options =>
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<DAL<Player>>();
 
 var app = builder.Build();
 
