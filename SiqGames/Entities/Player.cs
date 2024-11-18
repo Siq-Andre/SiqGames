8832661854a7d24fdff20c48068544411c4cf7e1
André Siqueira <andre.siqueira@itlab.com.br>
2024-11-18 09:24:03 -0300

    Changed list to icollection

--- SiqGames/Entities/Player.cs
+++ SiqGames/Entities/Player.cs
@@ -5,11 +5,11 @@
         public string Nickname { get; set; }
         public string FullName { get; set; }
         public string Email { get; set; }
-        public List<PlayerGame>? PlayerGames { get; set; }
+        public ICollection<PlayerGame>? PlayerGames { get; set; }
         public ICollection<Sale>? Sales { get; set; }
         public DateOnly BirthDate { get; set; }
-        public List<PlayerFriend>? Player1Friends { get; set; }
-        public List<PlayerFriend>? Player2Friends { get; set; }
-        public List<PlayerStudio>? PlayerStudios { get; set; }
+        public ICollection<PlayerFriend>? Player1Friends { get; set; }
+        public ICollection<PlayerFriend>? Player2Friends { get; set; }
+        public ICollection<PlayerStudio>? PlayerStudios { get; set; }
     }
 }
