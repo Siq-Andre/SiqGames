36550ec8280b52623bdffa5086d2a55a7c9bbca1
André Siqueira <andre.siqueira@itlab.com.br>
2024-11-25 14:03:01 -0300

    Player controller working

--- SiqGames/Controllers/PlayerController.cs
+++ SiqGames/Controllers/PlayerController.cs
@@ -22,7 +22,7 @@ namespace SiqGames.Controllers
         {
             if (playerViewModel == null)
             {
-                return BadRequest("Player data is required.");
+                return BadRequest(new { message = "Player data is required." });
             }
 
             try
@@ -58,7 +58,6 @@ namespace SiqGames.Controllers
             }
             catch (Exception ex)
             {
-                var x = 1;
                 return StatusCode(500, new { message = "Internal server error:" + ex.Message });
             }
         }
@@ -68,19 +67,19 @@ namespace SiqGames.Controllers
         {
             try
             {
-                var player = context.Set<Player>().ToList();
+                var player = context.Players.ToList();
                 return Ok(player);
             }
             catch (Exception ex)
             {
-                return StatusCode(500, "Internal server error:" + ex.Message);
+                return StatusCode(500, new { message = "Internal server error:" + ex.Message });
             }
         }
 
         [HttpGet("select/{id}")]
         public IActionResult GetPlayerById(int id)
         {
-            var Player = context.Set<Player>().FirstOrDefault(a => a.Id.Equals(id));
+            var Player = context.Players.FirstOrDefault(a => a.Id.Equals(id));
             if (Player == null)
             {
                 return NotFound();
@@ -89,35 +88,50 @@ namespace SiqGames.Controllers
         }
 
         [HttpPut("update/{id}")]
-        public IActionResult UpdatePlayer(int id, [FromBody] Player player)
+        public IActionResult UpdatePlayer(int id, [FromBody] PlayerRequestViewModel playerViewModel)
         {
-            if (player == null)
+            if (playerViewModel == null)
             {
-                return BadRequest("Player data is invalid.");
+                return BadRequest(new {message = "Player data is invalid." });
             }
 
-            var existingPlayer = context.Set<Player>().FirstOrDefault(a => a.Id.Equals(id));
+            var existingPlayer = context.Players.FirstOrDefault(a => a.Id.Equals(id));
             if (existingPlayer == null)
             {
                 return NotFound();
             }
 
-            existingPlayer.Nickname = player.Nickname;
-            existingPlayer.FullName = player.FullName;
-            existingPlayer.Email = player.Email;
-            existingPlayer.BirthDate = player.BirthDate;
-            existingPlayer.UserModified = player.UserModified;
-            existingPlayer.IsActive = player.IsActive;
+            existingPlayer.Nickname = playerViewModel.Nickname;
+            existingPlayer.FullName = playerViewModel.FullName;
+            existingPlayer.Email = playerViewModel.Email;
+            existingPlayer.BirthDate = playerViewModel.BirthDate;
+            existingPlayer.UserModified = "Admin";
+            existingPlayer.DateTimeModified = DateTime.Now;
+            existingPlayer.IsActive = true;
 
             try
             {
-                context.Set<Player>().Update(existingPlayer);
+
+                var checkPlayerExists = context.Players.FirstOrDefault(p => p.Id != existingPlayer.Id && (p.Nickname == existingPlayer.Nickname || p.Email == existingPlayer.Email));
+
+
+                if (checkPlayerExists != null && checkPlayerExists.Nickname == playerViewModel.Nickname)
+                {
+                    return Conflict(new { message = "Nickname already exists" });
+                }
+
+                else if (checkPlayerExists != null && checkPlayerExists.Email == playerViewModel.Email)
+                {
+                    return Conflict(new { message = "Email already exists" });
+                }
+
+                context.Players.Update(existingPlayer);
                 context.SaveChanges();
                 return Ok(existingPlayer);
             }
             catch (Exception ex)
             {
-                return StatusCode(500, $"An error occurred while updating the playerViewModel: {ex.Message}");
+                return StatusCode(500, new { message = "Internal server error:" + ex.Message });
             }
 
         }
@@ -125,13 +139,13 @@ namespace SiqGames.Controllers
         [HttpDelete("delete/{id}")]
         public IActionResult DeletePlayer(int id)
         {
-            var player = context.Set<Player>().FirstOrDefault(a => a.Id.Equals(id));
+            var player = context.Players.FirstOrDefault(a => a.Id.Equals(id));
             if (player == null)
             {
                 return NotFound();
             }
 
-            context.Set<Player>().Remove(player);
+            context.Players.Remove(player);
             context.SaveChanges();
 
             return Ok(player);
