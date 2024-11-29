5a951ce5dfbd0d5145fefd89911b42aed3a3c704
André Siqueira <andre.siqueira@itlab.com.br>
2024-11-29 08:55:46 -0300

    Fix player configuration typo

---
+++ SiqGames/Migrations/20241129115354_FixTypoInPlayerId.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SiqGames.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixTypoInPlayerId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "GameId",
+                table: "Players",
+                newName: "PlayerId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PlayerId",
+                table: "Players",
+                newName: "GameId");
+        }
+    }
+}
