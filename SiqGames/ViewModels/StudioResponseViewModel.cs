9dace834c37a77a155b4510d9509dfad6e902831
André Siqueira <andre.siqueira@itlab.com.br>
2024-12-10 10:17:23 -0300

    Response view model for Studios

---
+++ SiqGames/ViewModels/StudioResponseViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace SiqGames.ViewModels
+{
+    public class StudioResponseViewModel
+    {
+        public int StudioId { get; set; }
+        public string StudioName { get; set; }
+        public DateTime DateTimeCreated { get; set; }
+        public string UserCreated { get; set; }
+        public DateTime DateTimeModified { get; set; }
+        public string UserModified { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
